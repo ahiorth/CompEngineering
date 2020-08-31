dc7d746bec4dc0714574749dbead1c8c9d6fef9b
ahiorth <aksel.hiorth@uis.no>
2020-08-31 10:10:28 +0200

    added project1 and updated chapter on linear systems

---
+++ sphinx/_static/documentation_options.js
@@ -0,0 +1,11 @@
+var DOCUMENTATION_OPTIONS = {
+    URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
+    VERSION: 'Aksel Hiorth, the National IOR Centre &amp; Institute for Energy Resources,',
+    LANGUAGE: '1.0',
+    COLLAPSE_INDEX: false,
+    BUILDER: 'html',
+    FILE_SUFFIX: '.html',
+    HAS_SOURCE: true,
+    SOURCELINK_SUFFIX: '.txt',
+    NAVIGATION_WITH_KEYS: false
+};
