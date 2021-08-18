b94edd5ffcf21b7684467765482b253241f57863
ahiorth <aksel.hiorth@uis.no>
2021-08-18 10:53:23 +0200

    First commit fall 2021

---
+++ sphinx/_static/documentation_options.js
@@ -0,0 +1,12 @@
+var DOCUMENTATION_OPTIONS = {
+    URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
+    VERSION: 'Aksel Hiorth, the National IOR Centre &amp; Institute for Energy Resources, University of Stavanger',
+    LANGUAGE: '1.0',
+    COLLAPSE_INDEX: false,
+    BUILDER: 'html',
+    FILE_SUFFIX: '.html',
+    LINK_SUFFIX: '.html',
+    HAS_SOURCE: true,
+    SOURCELINK_SUFFIX: '.txt',
+    NAVIGATION_WITH_KEYS: false
+};
