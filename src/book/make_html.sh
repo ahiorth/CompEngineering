911111bdd069d09b2e55858baa7cafac3c8f481a
ahiorth <aksel.hiorth@uis.no>
2026-02-09 09:53:52 +0100

    cleaned up html

--- src/book/make_html.sh
+++ src/book/make_html.sh
@@ -31,35 +31,10 @@ html_style=bootswatch_journal
 system doconce format html $name $opt --html_style=$html_style  --html_code_style=inherit --html_output=$name
 #system doconce format html $name $opt --html_style=solarized_dark --html_code_style=inherit --html_output=$name
 #system doconce replace '\\\]' '\\]' $name.html
-system doconce replace 'width=400' 'width=800' $name.html
-system doconce split_html $name.html
-
-# Compile standard sphinx
-theme=alabaster
-#theme=scipy_lectures
-system doconce format sphinx $name $opt --sphinx_keep_splits
-system doconce replace 'width=400' 'width=800' $name.html
-system doconce split_rst $name
+system doconce replace 'width="400"' 'width="800"' $name.html
+#system doconce split_html $name.html
 
 
-
-#system python my_automake_sphinx.py
-
-# Generate and compile RunestoneInteractive book
-# (temporarily not available after their setup changed)
-# see bin/doconce and update generation of automake_sphinx.py
-#system doconce format sphinx $name --runestone $opt --sphinx_keep_splits -DRUNESTONE
-#system doconce split_rst $name
-#system doconce sphinx_dir theme=cbc dirname=runestone $name
-#system python automake_sphinx.py --runestone
-html_files="._book001.html ._book002.html ._book003.html ._book004.html ._book005.html"
-for htmf in $html_files; do
-    dir=sphinx-${theme}/_build/html/
-    # slash important for copying files in links to dirs
-    if [ -d $dir ]; then
-	doconce replace "\\\]" "\\]" $dir/$htmf
-    fi
-done
 # Publish
 dest=../../html
 if [ ! -d $dest ]; then
